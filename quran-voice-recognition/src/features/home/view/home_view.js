f9771fa6b5b2253d67fca6ad91b4181776998b9e
devmoaid1 <moaidmohamed1312@gmail.com>
2024-10-15 21:20:43 +0300

    changed dot indicatior color

--- quran-voice-recognition/src/features/home/view/home_view.js
+++ quran-voice-recognition/src/features/home/view/home_view.js
@@ -2,7 +2,7 @@ import Footer from "./components/footer";
 import Header from "./components/header";
 import InfoSection from "./components/info_section";
 import RecordingSection from "./components/recording_section";
-import AudioRecorder from "./components/recording_section";
+
 
 
 function HomeView() {
